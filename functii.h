662c127841f590f078b716e250d2228766ae0eac
Corches Ciprian <cipriancorches@gmail.com>
2023-02-23 20:29:29 +0200

    problema 3

--- functii.h
+++ functii.h
@@ -23,3 +23,121 @@ void afisare(int x[100][100], int& m, int& n) {
 		cout << endl;
 	}
 }
+bool numarCuCifreBinare(int n) {
+	bool aff = false;
+	while (n != 0) {
+		int cifra = n % 10;
+		if (cifra == 0 || cifra == 1) {
+			aff = true;
+		}
+		else {
+			return false;
+		}
+		n = n / 10;
+	}
+	return aff;
+}
+int nrElementeCuCifreBinare(int x[100][100], int& m, int& n) {
+	int nr = 0;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (numarCuCifreBinare(x[i][j]) == true) {
+				nr++;
+			}
+		}
+	}
+	return nr;
+}
+void afisareSol2(int x[100][100], int& m, int& n) {
+	int cst = 0;
+	for (int i = 0; i < m; i++) {
+		if (cst == 0) {
+			for (int j = 0; j < n; j++) {
+				cout << x[i][j] << " ";
+			}
+			cst = 1;
+		}
+		else if (cst == 1) {
+			for (int f = n-1; f >= 0; f--) {
+				cout << x[i][f] << " ";
+			}
+			cst = 0;
+		}
+	}
+	cout << endl;
+}
+int nrLiniiSimetrice(int x[100][100], int& m, int& n) {
+	int nr = 0;
+	for (int i = 0; i < m; i++) {
+		bool aff = false;
+		for (int j = 0; j < (n-1)/2; j++) {
+			for (int f = n; f > (n - 1) / 2; f--) {
+				if (x[i][j] == x[i][f]) {
+					aff = true;
+				}
+				else {
+					aff = false;
+				}
+			}
+		}
+		if (aff == true) {
+			nr++;
+		}
+	}
+	return nr;
+}
+int oglindit(int n) {
+	int ogl = 0;
+	while (n != 0) {
+		int cifra = n % 10;
+		ogl = ogl * 10 + cifra;
+		n = n / 10;
+	}
+	return ogl;
+}
+int celMaiMareDivComun(int a, int b) {
+	int dmaxA = -1;
+	int dmaxB = -1;
+	int dmaxCOM = 0;
+	int x1 = 1;
+	int x2 = 1;
+	for (x1 = 1, x2 = 1; x1 <= a, x2 <= b; x1++, x2++) {
+		if (a % x1 == 0) {
+			if (x1 > dmaxA) {
+				dmaxA = x1;
+			}
+		}
+		if (b % x2 == 0) {
+			if (x2 > dmaxB) {
+				dmaxB = x2;
+			}
+		}
+		if (dmaxA == dmaxB) {
+			dmaxCOM = dmaxA;
+		}
+	}
+	return dmaxCOM;
+}
+bool nrPimCuOgl(int n) {
+	int ogl = oglindit(n);
+	if (celMaiMareDivComun(n, ogl) == 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool toateElementeleDinMatricePrimeCuOgl(int x[100][100], int& m, int& n) {
+	bool aff = false;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < (n - 1) / 2; j++) {
+			if (nrPimCuOgl(x[i][j]) == true) {
+				aff = true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return aff;
+}
--- solutii.h
+++ solutii.h
@@ -1,3 +1,36 @@
 #pragma once
 #include "functii.h"
 using namespace std;
+
+//ex3 \/
+void sol1() {
+	//(3b) Care este numarul de elemente din matrice ce au in componenta doar cifre binare?
+	int x[100][100], m = 0, n = 0;
+	citire(x, m, n);
+	int nr = nrElementeCuCifreBinare(x, m, n);
+	cout << "Numarul de elemente cu cifre binare din vector este: " << nr << endl;
+}
+void sol2() {
+	//(3d) Afisati elementele matircei, pe o linie in ordinea din desen: (--> ; <--; -->)
+	int x[100][100], m = 0, n = 0;
+	citire(x, m, n);
+	afisareSol2(x, m, n);
+}
+void sol3() {
+	//(3e) Cate linii simetrice are matricea? O linie este simetrica daca elementele egal departate de capetele liniei sunt egale. Daca numarul are 5 cifre, cifra din mijloc va fi comparata cu ea insasi.
+	int x[100][100], m = 0, n = 0;
+	citire(x, m, n);
+	int nrl = nrLiniiSimetrice(x, m, n);
+	cout << "Numarul de linii simetrice in matrice este: " << nrl << endl;
+}
+void sol4() {
+	//(3f) Sunt toate elementele prime intre ele cu rasturnatul lor?
+	int x[100][100], m = 0, n = 0;
+	citire(x, m, n);
+	bool aff = toateElementeleDinMatricePrimeCuOgl(x, m, n);
+	aff == true
+		?
+		cout << "Toate elementele din matrice sunt prime intre ele cu rasturnatul lor" << endl
+		:
+		cout << "Nu toate elementele din matrice sunt prime intre ele cu rasturnatul lor" << endl;
+}
